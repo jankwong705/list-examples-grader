64c841a12455bc410e52d51e5c94091572cd5276
jankwong705 <114449531+jankwong705@users.noreply.github.com>
2022-11-10 16:55:09 -0800

    hi

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,58 @@
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import org.junit.*;
 
+class StringMethods implements StringChecker {
+  public boolean checkString(String s) {
+      return s.contains("s");
+  }
+}
+
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test
+  public void testFilter() {
+      StringChecker sc = new StringMethods();
+      List<String> s = new ArrayList<>();
+      List<String> expected = new ArrayList<>();
+      s.add("seasame");
+      expected.add("seasame");
+      s.add("sonata");
+      expected.add("sonata");
+      s.add("diego");
+      s.add("sofa");
+      expected.add("sofa");
+      s.add("detrimental");
+      s.add("coffee");
+      List<String> results = ListExamples.filter(s, sc);
+      assertEquals(expected, results);
+  }
+
+  @Test
+  public void testMerge() {
+      List<String> s1 = new ArrayList<>();
+      List<String> s2 = new ArrayList<>();
+      s1.add("apple");
+      s1.add("banana");
+      s1.add("chaisma");
+      s1.add("dormitory");
+      s2.add("concerning");
+      s2.add("dimension");
+      s2.add("event");
+      s2.add("fancy");
+      List<String> expected = new ArrayList<>();
+      expected.add("apple");
+      expected.add("banana");
+      expected.add("chaisma");
+      expected.add("concerning");
+      expected.add("dimension");
+      expected.add("dormitory");
+      expected.add("event");
+      expected.add("fancy");
+      List<String> results = ListExamples.merge(s1, s2);
+      assertEquals(expected, results);
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -3,5 +3,43 @@
 set -e
 
 rm -rf student-submission
+
 git clone $1 student-submission
+
+cp TestListExamples.java student-submission
+cd student-submission
+
+CPATH=.:list-examples-grader/lib/hamcrest-core-1.3.jar:list-examples-grader/lib/junit-4.13.2.jar
+
+if [[ -f ListExamples.java ]]
+then 
+    echo "File Found!"
+else
+    echo "File Not Found!"
+    exit
+fi
+
+set +e
+
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]]
+then
+    echo "Successful Compile!"
+else
+    echo "Compile Error!"
+    exit
+fi
+
+java -cp $CPATH TestListExamples 
+
+if [[ $? -eq 0 ]]
+then
+    echo "Test Passed!"
+else
+    echo "Tests Error!"
+    exit
+fi
+
+
 
